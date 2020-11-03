0690842d294c5a1a446dba55c6f31d9b2f121853
timbotimber <hitimstephens@gmail.com>
2020-11-03 15:55:30 +0100

    this works

--- js/game.js
+++ js/game.js
@@ -1,71 +1,70 @@
 class Game {
-    constructor() {}
-    preloadGame() {
-      //console.log("this is the game preload");
-      this.backgroundImages = [
-        { src: loadImage("../assets/background/turkis-1.png"), x: 0, speed: 0 },
-        { src: loadImage("../assets/background/plx-2.png"), x: 0, speed: 1 },
-        { src: loadImage("../assets/background/plx-3.png"), x: 0, speed: 2 },
-        { src: loadImage("../assets/background/plx-4.png"), x: 0, speed: 3 },
-        { src: loadImage("../assets/background/plx-5.png"), x: 0, speed: 4 },
-      ];
-      this.playerImage = loadImage("../assets/player/mr-player-fly.png");
-      this.playbuttonImage = loadImage("../assets/playbutton/playbutton.png");
-      this.popcornImage = loadImage("../assets/player/mr-player-fly.png");
-      console.log("Here is the popcorn:", this.popcornImage);
-    }
-    setupGame() {
-      //console.log("this is the game setup");
-      //  initialize background + player here
-      // NB: we DON'T initialize the coins here because we create them dynamically in the draw
-      this.background = new Background(this.backgroundImages);
-      this.player = new Player(this.playerImage);
-      this.playbuttons = [];
-      this.popcorns = [];
-      this.testPopcorn = new Popcorn(this.popcornImage);
-      console.log(this.testPopcorn);
-    }
-  
-    drawGame() {
-      // console.log("this is the game draw");
-      this.background.drawBackground();
-      this.player.drawPlayer();
-      if (frameCount % 270 === 0) {
-        //console.log("this will be the push event");
-        this.playbuttons.push(new Playbutton(this.playbuttonImage));
-        //console.log(this.playbuttons);
-      }
-      // if (frameCount % 360 === 0) {
-      //   //console.log("this will be the popcorn push event");
-      //   this.popcorns.push(new Popcorn(this.popcornImage));
-      //   //console.log(this.popcorns);
-      // }
-  
-      this.playbuttons.forEach(function (playbutton) {
-        playbutton.drawPlaybutton();
-      });
-      //  call the draw functions for the player + the background
-      this.playbuttons = this.playbuttons.filter((playbutton) => {
-        if (playbutton.collision(this.player)) {
-          return false;
-        } else {
-          return true;
-        }
-      });
-      // define the obstacle drawing logic + add a new obstacle to  the array in the constructor with the image passed into it
-      
-      this.popcorns.forEach(function (popcorn) {
-        popcorn.drawPopocorn();
-      });
-      //  call the draw functions for the player + the background
-      this.popcorns = this.popcorns.filter((popcorn) => {
-        if (popcorn.collision(this.player)) {
-          return false;
-        } else {
-          return true;
-        }
-      });
-    
-    }
+  constructor() {}
+  preloadGame() {
+    //console.log("this is the game preload");
+    this.backgroundImages = [
+      { src: loadImage("../assets/background/turkis-1.png"), x: 0, speed: 0 },
+      { src: loadImage("../assets/background/plx-2.png"), x: 0, speed: 1 },
+      { src: loadImage("../assets/background/plx-3.png"), x: 0, speed: 2 },
+      { src: loadImage("../assets/background/plx-4.png"), x: 0, speed: 3 },
+      { src: loadImage("../assets/background/plx-5.png"), x: 0, speed: 4 },
+    ];
+    this.playerImage = loadImage("../assets/player/mr-player-fly.png");
+    this.playbuttonImage = loadImage("../assets/playbutton/playbutton.png");
+    this.popcornImage = loadImage("../assets/player/mr-player-fly.png");
+    console.log("Here is the popcorn:", this.popcornImage);
   }
-  //|| playbutton.x < 0
+  setupGame() {
+    //console.log("this is the game setup");
+    //  initialize background + player here
+    // NB: we DON'T initialize the coins here because we create them dynamically in the draw
+    this.background = new Background(this.backgroundImages);
+    this.player = new Player(this.playerImage);
+    this.playbuttons = [];
+    this.popcorns = [];
+    this.testPopcorn = new Popcorn(this.popcornImage);
+    console.log(this.testPopcorn);
+  }
+
+  drawGame() {
+    // console.log("this is the game draw");
+    this.background.drawBackground();
+    this.player.drawPlayer();
+    if (frameCount % 270 === 0) {
+      //console.log("this will be the push event");
+      this.playbuttons.push(new Playbutton(this.playbuttonImage));
+      //console.log(this.playbuttons);
+    }
+    if (frameCount % 360 === 0) {
+      //console.log("this will be the popcorn push event");
+      this.popcorns.push(new Popcorn(this.popcornImage));
+      //console.log(this.popcorns);
+    }
+
+    this.playbuttons.forEach(function (playbutton) {
+      playbutton.drawPlaybutton();
+    });
+    //  call the draw functions for the player + the background
+    this.playbuttons = this.playbuttons.filter((playbutton) => {
+      if (playbutton.collision(this.player)) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+    // define the obstacle drawing logic + add a new obstacle to  the array in the constructor with the image passed into it
+
+    this.popcorns.forEach(function (popcorn) {
+      popcorn.drawPopcorn();
+    });
+    //  call the draw functions for the player + the background
+    this.popcorns = this.popcorns.filter((popcorn) => {
+      if (popcorn.collision(this.player)) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+  }
+}
+//|| playbutton.x < 0
---
+++ js/popcorn.js
@@ -0,0 +1,30 @@
+class Popcorn {
+  constructor(popcornImage) {
+    this.image = popcornImage;
+    this.x = width;
+    this.y = (Math.random() * height) / 2;
+    this.width = 50;
+    this.height = 50;
+    // define the images,size and x/y values in the constructor
+  }
+  //   define the collision function
+  collision(playerInfo) {
+    console.log("this will be the popcorn collision", playerInfo);
+    let popcornX = this.x + this.width / 2;
+    let popcornY = this.y + this.height / 2;
+    let playerX = playerInfo.x + playerInfo.width / 2;
+    let playerY = playerInfo.y + playerInfo.height / 2;
+    if (dist(popcornX, popcornY, playerX, playerY) > 50) {
+      console.log(game.player.score);
+      return false;
+    } else {
+      game.player.score -= 10;
+      return true;
+    }
+  }
+  drawPopcorn() {
+    this.x--;
+    // this.collision(game.player);
+    image(this.image, this.x, this.y, this.width, this.height);
+  }
+}
